057156eea17ed3ef3c134845c45a6ad748e1126f
League Student <league@iMac-34.attlocal.net>
2022-05-25 16:49:58 -0700

    Create VotingBooth.java

---
+++ src/_03_string_conversion/_4_voting_booth/VotingBooth.java
@@ -0,0 +1,38 @@
+package _03_string_conversion._4_voting_booth;
+
+import javax.swing.JOptionPane;
+
+public class VotingBooth {
+
+	// 1) Make a main method that includes all the steps below…. 
+	public static void main(String[] args) {
+
+		
+		String age = JOptionPane.showInputDialog("Hwo aolsd aer tpouap!");
+		
+		
+		int ageInt = Integer.parseInt(age);	
+		
+		if (ageInt == 18)
+		{
+			JOptionPane.showMessageDialog(null, "Yasdofyasaydfad! Youo acn waavoet aes!");
+		}
+		else {
+		JOptionPane.showMessageDialog(null, "Novayaud caers a baotu ahwoat ypuo thinkl.......");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+}
+}
